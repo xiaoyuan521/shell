0e49ff18a798212d5f51f7006f0dbcb37815dd8d
yisurenali@aliyun.com <yisurenali@aliyun.com>
2016-06-13 13:02:29 +0800

    add float value calc

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # 将tsv文件读入到数组中
-while read line
-do
-  line=`echo $line | sed -r "s/\t/,/g"`
-  IFS=",";line_arr=($line)
-  for i in ${line_arr[@]}; do
-    echo $i
-  done
-done
+# while read line
+# do
+#   line=`echo $line | sed -r "s/\t/,/g"`
+#   IFS=",";line_arr=($line)
+#   for i in ${line_arr[@]}; do
+#     echo $i
+#   done
+# done
 
 # 关于echo显示字符串的问题
 # 如果想保持字符串原样，需要使用双引号
@@ -17,3 +17,24 @@ a="111  222 333"
 echo "$a"
 echo $a
 
+#### 数值的运算（浮点数）（正负均可） ####
+echo "0.1 - 0.3" | bc | sed -r 's/^(-?)\./\10./'
+
+#### 数值的比较 ####
+# 非浮点数的比较方法
+a="111" 
+b="222" 
+if [ "$a" -lt "$b" ]; then
+  echo "a 小于 b" 
+else
+  echo "a 不小于 b" 
+fi
+
+# 浮点数的比较方法，正数，负数均可
+a="0.111" 
+b="0.112" 
+if [ `echo "$a < $b" | bc` -eq 1 ]; then
+  echo "a 小于 b" 
+else
+  echo "a 不小于 b" 
+fi
